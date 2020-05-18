c9060992bc87f47aca9d93adc525ffb254b2309d
OreOlad <65356045+OreOlad@users.noreply.github.com>
2020-05-18 19:26:55 -0400

    5kyu Javascript answer

---
+++ Javascript Codewars answers/Calculating with functions.js
@@ -0,0 +1,74 @@
+function zero(x) {
+  if (x == null) {
+    return 0;
+  }else{
+    return Math.floor(eval("0" + x));
+  }
+}
+function one(x) {
+  if (x == null) {
+    return 1;
+  }else{
+    return Math.floor(eval("1" + x));
+  }
+}
+function two(x) {
+  if (x == null) {
+    return 2;
+  }else{
+    return Math.floor(eval("2" + x));
+  }
+}
+function three(x) {
+  if (x == null) {
+    return 3;
+  }else{
+    return Math.floor(eval("3" + x));
+  }
+}
+function four(x) {
+  if (x == null) {
+    return 4;
+  }else{
+    return Math.floor(eval("4" + x));
+  }
+}
+function five(x) {
+  if (x == null) {
+    return 5;
+  }else{
+    return Math.floor(eval("5" + x));
+  }
+}
+function six(x) {
+  if (x == null) {
+    return 6;
+  }else{
+    return Math.floor(eval("6" + x));
+  }
+}
+function seven(x) {
+  if (x == null) {
+    return 7;
+  }else{
+    return Math.floor(eval("7" + x));
+  }
+}
+function eight(x) {
+  if (x == null) {
+    return 8;
+  }else{
+    return Math.floor(eval("8" + x));
+  }
+}
+function nine(x) {
+  if (x == null) {
+    return 9;
+  }else{
+    return Math.floor(eval("9" + x));
+  }
+}
+function plus(x) {return "+" + x.toString();}
+function minus(x) {return "-" + x.toString();}
+function times(x) {return "*" + x.toString();}
+function dividedBy(x) {return "/" + x.toString();}
