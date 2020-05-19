047d9036ffc50d7c1492b05aec66c9f0d72458a7
OreOlad <65356045+OreOlad@users.noreply.github.com>
2020-05-18 20:31:39 -0400

    Delete IQ test.js

--- Javascript Codewars answers/IQ test.js
+++
@@ -1,7 +0,0 @@
-function iqTest(numbers){
-  var nums = numbers.split(" ").map(x => x % 2);  
-  var sum = nums.reduce((a,b) => a + b);  
-  var target = sum > 1 ? 0 : 1;
-
-  return nums.indexOf(target) + 1;
-}
